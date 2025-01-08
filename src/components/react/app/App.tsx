8782995854fce0084928505e85e508cbee2006f7
igorraknorwid <igorraknorwid@gmail.com>
2025-01-08 13:24:34 +0100

    sass init

--- src/components/react/app/App.tsx
+++ src/components/react/app/App.tsx
@@ -9,7 +9,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1 className="title">Заголовок с React</h1>
+      <h1 className="title">React</h1>
       <h2 className="title">{store.title&&store.title}</h2>
     </main>
   );
